6d57d4f010c92a54eee46b30b2f13ba737b47a7d
CoderJin <15558188801jt@gmail.com>
2017-06-22 06:21:53 +0000

    Move user model to models folder

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,7 +4,7 @@
   <div class="jumbotron">
     <h1>Hello Laravel</h1>
     <p class="lead">
-      你现在所看到的是 <a href="https://coderjin.com">Laravel 入门教程</a>
+      你现在所看到的是 <a href="http://coderjin.com">Laravel 入门教程</a>
       的示例项目主页。
     </p>
     <p>
