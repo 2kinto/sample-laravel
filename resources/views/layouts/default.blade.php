54ad926fc8bbbbd082b7deb1958a696cd1803972
CoderJin <15558188801jt@gmail.com>
2017-06-22 03:08:20 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,9 +2,16 @@
 <html>
 <head>
     {{--yield 有两个参数 第一个是该区块的变量名 第二个参数是默认值--}}
-    <title>@yield('title','Sample')- Laravel</title>
+    <title>@yield('title','Sample App')- Laravel</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+    @include('layouts._header')
+
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10"></div>
+        @yield('content')
+        @include('layouts._footer')
+    </div>
 </body>
 </html>
